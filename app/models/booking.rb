bff5297885813b519d8d275bf175eac0312d62c7
Rokas Vaitkevicius <rokas.vaitkevicius1@gmail.com>
2019-08-19 16:12:42 +0100

    Deleted all the destroy dependencies

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :venue
-  has_many :venue_reviews, dependent: :destroy
+  has_many :venue_reviews
   validates :price, :check_in, :check_out, presence: true
 end
