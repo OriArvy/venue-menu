5974bcc9d68ead7f4ea14df0137244707a7f2d6b
Thomas Britz <britzthomas@gmail.com>
2019-08-20 17:28:17 +0100

    random

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -31,12 +31,12 @@ class VenuesController < ApplicationController
   def update
     @venue = Venue.find(params[:id])
     if @venue.update(venue_params)
-      redirect_to venue_path(venue)
+      redirect_to venue_path(@venue)
     else
       render :new
     end
   end
-  
+
   def destroy
     @venue = Venue.find(params[:id])
     @venue.delete
