bb20afe1c363406e1a8bed6e3c881330668a7862
Ben Whitehouse <benw27@gmail.com>
2019-08-22 17:39:15 +0100

    show and review updates

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,7 @@
 class Venue < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_many :venue_reviews, through: :bookings
   validates :name, :price, :address, :description, presence: true
   validates :name, :address, uniqueness: true
   mount_uploader :photo, EventUploader
