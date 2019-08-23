df4454ed08b2d460eeecf96dd35c443ccf0684e7
matt annetts <mot182@hotmail.com>
2019-08-23 11:18:09 +0100

    autocomplete on search bars

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,8 @@ import { initStarRating } from '../plugins/init_star_rating';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 import { previewImageOnFileSelect } from '../components/photo_preview'
+import { initAutocomplete } from '../plugins/init_autocomplete'
 previewImageOnFileSelect();
 initMapbox();
 initStarRating();
+initAutocomplete();
